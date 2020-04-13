aed785b7af5301ebe8c98a713d8b6c0508bd9c59
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-13 14:23:02 +0530

    constants

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const activeCardColor = Color(0xFF1D1E33);
+const inactiveCardColor = Color(0xFF111328);
+
+enum Gender { male, female }
+
+const TextStyle cardtextstyle=TextStyle(fontSize: 18, color: Color(0xFF8D8E98));
