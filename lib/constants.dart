84ea38b93f8b47cea67dc08dd0eaf3fdfc9238c9
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-13 15:57:10 +0530

    buttonColor

--- lib/constants.dart
+++ lib/constants.dart
@@ -7,3 +7,5 @@ enum Gender { male, female }
 
 const TextStyle cardtextstyle=TextStyle(fontFamily: 'Poppins',fontSize: 18, color: Color(0xFF8D8E98));
 const TextStyle boldCardHeadings=TextStyle(fontFamily: 'Poppins',fontSize: 50, fontWeight: FontWeight.bold);
+
+const Color buttonColor=Color(0xFF4C4F5E);
