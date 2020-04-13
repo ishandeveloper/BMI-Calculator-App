db25be1b0ef60ab51a4d916c54832efc410ee24b
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-13 13:04:53 +0530

    Male Card

--- lib/pages/input.dart
+++ lib/pages/input.dart
@@ -1,5 +1,6 @@
 import 'package:bmi_calculator/widgets/customcard.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class InputPage extends StatefulWidget {
   @override
@@ -24,6 +25,22 @@ class _InputPageState extends State<InputPage> {
               children: <Widget>[
                 BMICard(
                   colour: cardColor,
+                  cardChild: Column(
+                    children: <Widget>[
+                      Icon(
+                        FontAwesomeIcons.male,
+                        size: 80,
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        'MALE',
+                        style:
+                            TextStyle(fontSize: 18, color: Color(0xFF8D8E98)),
+                      )
+                    ],
+                  ),
                 ),
                 BMICard(
                   colour: cardColor,
