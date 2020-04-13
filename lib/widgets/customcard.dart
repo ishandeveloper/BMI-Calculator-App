81d9587cf75eb56a1deae2cba50d39b2bb04deee
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-13 12:59:38 +0530

    card content

--- lib/widgets/customcard.dart
+++ lib/widgets/customcard.dart
@@ -4,7 +4,7 @@ class BMICard extends StatelessWidget {
   // const BMICard({
   //   Key key,
   // }) : super(key: key);
-  BMICard({@required this.colour});
+  BMICard({@required this.colour, this.cardChild});
   final Color colour;
   final Widget cardChild;
 
@@ -12,6 +12,7 @@ class BMICard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Expanded(
       child: Container(
+        child: cardChild,
         margin: EdgeInsets.all(10),
         decoration: BoxDecoration(
           color: colour,
