aad6560f24ae4fda67ddbd8c98b756f51e25b52d
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-13 14:23:16 +0530

    height card

--- lib/pages/input.dart
+++ lib/pages/input.dart
@@ -2,11 +2,7 @@ import 'package:bmi_calculator/widgets/cardiconcontent.dart';
 import 'package:bmi_calculator/widgets/customcard.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-
-const activeCardColor = Color(0xFF1D1E33);
-const inactiveCardColor = Color(0xFF111328);
-
-enum Gender { male, female }
+import '../constants.dart';
 
 class InputPage extends StatefulWidget {
   @override
@@ -23,6 +19,7 @@ class _InputPageState extends State<InputPage> {
         elevation: 0,
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Expanded(
             child: Row(
@@ -61,6 +58,12 @@ class _InputPageState extends State<InputPage> {
           Expanded(
             child: BMICard(
               colour: activeCardColor,
+              cardChild: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text('HEIGHT',style: cardtextstyle,)
+                ],
+              ),
             ),
           ),
           Expanded(
