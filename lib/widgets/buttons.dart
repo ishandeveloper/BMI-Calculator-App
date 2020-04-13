80e5e22b3c19869a4581ae58afeb67a0778cc685
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-13 15:57:28 +0530

    custom button

---
+++ lib/widgets/buttons.dart
@@ -0,0 +1,23 @@
+import 'package:bmi_calculator/constants.dart';
+import 'package:flutter/material.dart';
+
+
+class CustomIconButton extends StatelessWidget {
+
+  final IconData icon;
+  final Function pressed;
+
+  CustomIconButton({this.icon,this.pressed});
+  
+  @override
+  Widget build(BuildContext context) {
+    return RawMaterialButton(
+        onPressed: pressed,
+        shape: CircleBorder(),
+        fillColor: buttonColor,
+        // elevation: 5,
+        child: Icon(icon),
+        padding: EdgeInsets.all(16),
+    );
+  }
+}
