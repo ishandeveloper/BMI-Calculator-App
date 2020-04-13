38572a656f082652c1689c79fbfaeed7bacb2e49
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-13 12:30:07 +0530

    required

--- lib/widgets/customcard.dart
+++ lib/widgets/customcard.dart
@@ -4,7 +4,7 @@ class BMICard extends StatelessWidget {
   // const BMICard({
   //   Key key,
   // }) : super(key: key);
-  BMICard({this.colour});
+  BMICard({@required this.colour});
   final Color colour;
 
 
